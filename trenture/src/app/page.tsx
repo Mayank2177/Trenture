2b85d54feba3c5cdfab31a752a8d6fd68fddbbe3
sDaCoder <supravadutta_sDa@outlook.com>
2025-08-14 10:33:34 +0530

    Started a nextjs project

---
+++ trenture/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center">
+        This is the root directory of Trenture
+      </h1>
+    </>
+  );
+}
